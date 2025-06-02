3ee9126d2d074ea411934ebc5c6e353e87b9552d
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-06-02 12:48:43 +0300

    Update emix-fun.js

--- plugins/emix-fun.js
+++ plugins/emix-fun.js
@@ -31,7 +31,7 @@ cmd({
         let buffer = await getBuffer(imageUrl);
         let sticker = new Sticker(buffer, {
             pack: "Emoji Mix",
-            author: "TOHID_MD",
+            author: "꧁༒☬ 𝕷𝕺𝕱𝕿-𝕏𝕸𝕯 ☬༒꧂",
             type: StickerTypes.FULL,
             categories: ["🤩", "🎉"],
             quality: 75,
