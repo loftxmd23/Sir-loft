87dd0c5fc2efcc7e76b32d5607cb42eb8db86fe1
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-06-02 13:25:36 +0300

    Update quote.js

--- plugins/quote.js
+++ plugins/quote.js
@@ -12,7 +12,7 @@ cmd({
     const response = await axios.get("https://api.quotable.io/random");
     const { content, author } = response.data;
 
-    const message = `💬 *"${content}"*\n- ${author}\n\n> *QUOTES BY TOHID_MD*`;
+    const message = `💬 *"${content}"*\n- ${author}\n\n> *『 ⚡𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝕾𝖎𝖗 𝕷𝖔𝖋𝖙⚡ 』*`;
     reply(message);
   } catch (error) {
     console.error("Error fetching quote:", error);
