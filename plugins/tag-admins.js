06b03db8e4e864c9c9e8cde42787163fb79c09a9
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-06-02 13:33:20 +0300

    Update tag-admins.js

--- plugins/tag-admins.js
+++ plugins/tag-admins.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
             teks += `${randomEmoji} @${admin.split('@')[0]}\n`;
         }
 
-        teks += "└──✪ 𝐌𝐒𝐄𝐋𝐀-𝐂𝐇𝐔𝐈 ┃ 𝐁𝐎𝐓 ✪──";
+        teks += "└──✪ loft ┃ xmd ✪──";
 
         conn.sendMessage(from, { text: teks, mentions: admins }, { quoted: mek });
 
