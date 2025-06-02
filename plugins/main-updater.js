754ce8891cf7ca421441b6b6e9ddf05d00153b78
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-06-02 13:12:13 +0300

    Update main-updater.js

--- plugins/main-updater.js
+++ plugins/main-updater.js
@@ -16,7 +16,7 @@ cmd({
     if (!isOwner) return reply("This command is only for the bot owner.");
 
     try {
-        await reply("🐯 Checking for 𝐌𝐒𝐄𝐋𝐀-𝐂𝐇𝐔𝐈-𝐁𝐎𝐓 updates...");
+        await reply(" Checking for ꧁༒☬ 𝕷𝕺𝕱𝕿-𝕏𝕸𝕯 ☬༒꧂ updates...");
 
         // Fetch the latest commit hash from GitHub
         const { data: commitData } = await axios.get("https://api.github.com/repos/Mselachui03/MSELA-CHUI-BOT/commits/main");
