c2ad04be701aaa61de6fde881aa0dbf899003c8a
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-06-09 04:04:43 +0300

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -80,7 +80,7 @@ cmd({
             try {
                 await new Promise(resolve => setTimeout(resolve, 1000)); // Small delay after image
                 await conn.sendMessage(from, {
-                    audio: { url: 'https://github.com/loftxmd23/sir-loft/raw/refs/heads/main/autovoice/menunew.m4a' },
+                    audio: { url: 'https://files.catbox.moe/mqdb3h.mp3' },
                     mimetype: 'audio/mp4',
                     ptt: true,
                 }, { quoted: mek });
