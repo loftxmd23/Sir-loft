b4efe043cfce19740c21d79f734c60ec1660a083
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-06-12 06:24:13 +0300

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -42,7 +42,16 @@ async (conn, mek, m, { from, sender, reply }) => {
                 }
             }
         }, { quoted: mek });
-
+       // Function to send menu audio with timeout
+        const sendMenuAudio = async () => {
+            try {
+                await new Promise(resolve => setTimeout(resolve, 1000)); // Small delay after image
+                await conn.sendMessage(from, {
+                    audio: { url: 'https://github.com/loftxmd23/Loft-base/raw/main/autovoice/loft.mp3' },
+                    mimetype: 'audio/mp4',
+                    ptt: true,
+                }, { quoted: mek });
+ 
     } catch (e) {
         console.error("Alive Error:", e);
         reply(`An error occurred: ${e.message}`);
